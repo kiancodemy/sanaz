949c7d52d83454b48e1873be0ff332d44461803b
kian kheirani <kiankheirani@gmail.com>
2024-12-04 12:40:15 +0330

    '2'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/sanaz/",
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
 });
